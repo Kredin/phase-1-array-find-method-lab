ee23077404bca0d0b904dbb24a5fd3804de37ed8
Blade <bladeprojectz@gmail.com>
2024-03-05 16:51:17 +0800

    lab complete

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // code your solution here
 
+// function superbowlWin(array) {
+//   if (array.find(isW) == "W") {
+//     return array.find(isW).year;
+//   } else {
+//     return undefined;
+//   }
+// }
 
-function superbowlWin(array){
-    return array.find(isW).year
-}
+// function isW(score) {
+//   return score.result === "W";
+// }
 
-function isW(score) {
-    return score.result === "W"
-}
-    
+// function superbowlWin(array) {
+//   let isW = array.find((e) => e.result === "W");
+//   if (isW === "year") {
+//     return undefined;
+//   } else {
+//     return isW.year;
+//   }
+// }
 
+superbowlWin = (record) => {
+  const result = record.find((record) => record.result === "W");
+  return !!result ? result.year : undefined;
+};
